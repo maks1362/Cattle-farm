a52880606f1f7e4e9a0fc25ffa1acea32a989eff
MichaelDeCortix <tka4enko.neznakomec@gmail.com>
2019-06-02 22:42:34 +0300

    Main fix

--- Doyki/MainForm.cs
+++ Doyki/MainForm.cs
@@ -29,83 +29,10 @@ namespace Doyki
         [DllImportAttribute("user32.dll")]
         private static extern bool ReleaseCapture();
 
-        //Переменные
-
-        //static Form form = MainForm.ActiveForm;
-        /*private static IList<string> dataTables = new List<string>
-            {
-                { "Скот" },//cattle
-                { "Доильные аппараты" },//milking machine
-                { "Лактации" },//и анализы молока
-                { "Анализы крови" },//blood analyses
-                { "Репродуктивность" }
-               // new Element() { Symbol="Sc", Name="Scandium", AtomicNumber=21}},
-            };*/
-
-
-
-
-        /// <summary>
-        /// Создаёт генеалогическое TreeView для коровы "cowId"
-        /// </summary>
-        /// <param name="cowId">Корова для которой строиться генеалогическое древо</param>
-        /// <param name="dataGrid">Анализируемая таблица с коровами</param>
-        /// <param name="nodeCountOut">Костыль для счёта общего количества узлов</param>
-        /// <returns></returns>
-        private TreeView MakeTree(int cowId, DataGridView dataGrid, out int nodeCountOut)
-        {
-            int nodeCount = 1;//Ещё один костыль, out переменные нельзя использовать во влож функциях
-            TreeView tree = treeView1; // new TreeView();
-            tree.Nodes.Add(new TreeNode(cowId.ToString()));
-            GetNodes(tree.Nodes[0], cowId.ToString());
-
-            nodeCountOut = nodeCount;
-            return tree;
-
-
-            ///Рекурсивная процедура создания генеологического древа для коровы
-            void GetNodes(TreeNode curNode, string id) 
-            {
-                if (id != null)
-                {
-                    int idGrid = -1;
-                    for (int i = 0; i < dataGrid.RowCount; i++)
-                    {
-                        if (dataGrid[0, i].Value.ToString() == id)
-                        {
-                            idGrid = i;
-                            break;
-                        }
-                    }
-
-                    string newNode = dataGrid[5, idGrid].Value.ToString();
-                    if (newNode != "")
-                    {
-                        curNode.Nodes.Add(newNode, newNode);
-                        nodeCount++;
-                        GetNodes(curNode.Nodes[newNode], newNode);
-                    }
-                    //else
-                        //MessageBox.Show("pizdaaaa2");
-                }
-                else
-                    MessageBox.Show("xyiii1");
-                return;
-
-            }
-        }
-
         public MainForm()
         {
             InitializeComponent();
             this.SetStyle(ControlStyles.ResizeRedraw, true);
-
-            CattleToolStripMenuItem_Click(null, null);
-            pictureBox1.Image = DrawBinaryTree(TableFormCattle.dataGridView1);//
-            
-
-            //dataGrid = ;
-
         }
 
         //Resize
@@ -124,7 +51,7 @@ namespace Doyki
                 Point screenPoint = new Point(m.LParam.ToInt32());
                 Point clientPoint = this.PointToClient(screenPoint);
 
-                Dictionary<UInt32, Rectangle> boxes = new Dictionary<UInt32, Rectangle>() {{HTBOTTOMRIGHT, new Rectangle(formSize.Width - RESIZE_HANDLE_SIZE, formSize.Height - RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE)}};
+                Dictionary<UInt32, Rectangle> boxes = new Dictionary<UInt32, Rectangle>() { { HTBOTTOMRIGHT, new Rectangle(formSize.Width - RESIZE_HANDLE_SIZE, formSize.Height - RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE) } };
 
                 foreach (KeyValuePair<UInt32, Rectangle> hitBox in boxes)
                 {
@@ -274,129 +201,6 @@ namespace Doyki
                 this.Text = "Выберите таблицу";
                 nameLabel.Text = "Выберите таблицу";
             }
-        }
-
-
-               
-        /// <summary>
-        /// Метод рисует графическое изображение двоичного дерева
-        /// </summary>
-        /// <returns>Объект Bitmap</returns>
-        Bitmap DrawBinaryTree(DataGridView dataGrid)
-        {
-            int nodeCount;//Содержит общее количество узлов дерева (и да, я знаю о treeView.VisibleCount, он не робит)
-            TreeView treeView = MakeTree(237745, dataGrid, out nodeCount);
-            
-            //*Это тестовый вариант, раскомментируй, чтобы попробовать 
-                treeView = new TreeView();
-                treeView.Nodes.Add("1");
-                treeView.Nodes[0].Nodes.Add("2");
-                treeView.Nodes[0].Nodes.Add("3");
-                treeView.Nodes[0].Nodes.Add("4");
-                treeView.Nodes[0].Nodes.Add("41");
-                treeView.Nodes[0].Nodes[0].Nodes.Add("5");
-                treeView.Nodes[0].Nodes[2].Nodes.Add("6");
-                treeView.Nodes[0].Nodes[3].Nodes.Add("61");
-                treeView.Nodes[0].Nodes[3].Nodes.Add("61");
-                treeView.Nodes[0].Nodes[3].Nodes[1].Nodes.Add("611");
-                treeView.Nodes[0].Nodes[0].Nodes.Add("7");
-                treeView.Nodes[0].Nodes[1].Nodes.Add("8");
-                treeView.Nodes[0].Nodes[1].Nodes.Add("9");
-                nodeCount = 13;
-            
-
-            /// Длина стороны прямоугольника изображающего вершину.
-            /// Я взял 11, исходя из рисунка. В реальности же, необходимо считать исходя из 
-            /// степени разветвлённости графа
-            int w = 20; // Math.Min(bmp.Width, bmp.Height) / 9;
-            Size s = new Size(w*3, w);
-
-            /// Задаём размер изображения, на котором будем рисовать
-            /// В данном случае привязка идёт к размерам PictureBox
-            /// Сам pictureBox хз как определить размер нужный
-            Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width + nodeCount * w*2, pictureBox1.ClientSize.Height + nodeCount * w);
-
-            /// Создаём Graphics из изображения
-            using (Graphics g = Graphics.FromImage(bmp))
-            {
-
-                using (Pen pen = new Pen(Color.Gray, 1.5f),// Перо для рисования прямоугольников
-                    arrowPen = new Pen(Color.Green, 1.5f) { EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor })
-                {//Перо для рисования стрелок
-                    g.TranslateTransform(10, 10);//Отступ по 10 пикселей от верхнего левого угла изображения
-                    /// Массив точек, задающих верхний левый угол прямоугольника                             
-                    Point[] points = new Point[nodeCount];
-                    points[0] = new Point(0, 0);
-                    int curPoint = 1;
-                    int maxX = 0;
-                    Get(treeView.Nodes[0], points[0]);
-                    DrawVertex(g, pen, treeView.Nodes[0].Text.ToString(), points[0], s);
-
-                    /// Рисуем стрелки между прямоугольниками
-                    //DrawArrows(g, arrowPen, points[0], points[1], s);
-
-                    ///Рекурсивная магическая процедура, сам не знаю до конца как работает. Если смотреть на рисунок конечный
-                    ///Обход происхдит сверху вниз по первой ветке, затем 1 узел наверх и вправо до конца, 1 узел наверх и вправо до конца
-                void Get(TreeNode treeNode, Point highPoint)
-                {
-                        int pointsAtLevel = 0;
-                        //maxPointsAtCol = 0;
-                    foreach (TreeNode tn in treeNode.Nodes)
-                    {
-                            ///Ставим точку, где будет узел дерева
-                        points[curPoint] = new Point(Convert.ToInt32(/*highPoint.X +*/ (pointsAtLevel * (w*4)) + (maxX)), highPoint.Y + w*3);
-                        DrawArrows(g, arrowPen, highPoint, points[curPoint], s);
-                            /// Рисуем прямоугольники. Это самое простое, если знать вершины
-                        DrawVertex(g, pen, tn.Text.ToString(), points[curPoint], s);
-                                pointsAtLevel++;
-                        if (points[curPoint].X - w > maxX)
-                            maxX = points[curPoint].X;
-                        if (curPoint < nodeCount)
-                        {
-                            Get(tn, points[curPoint++]);
-                        }
-                            //pointsAtLevel = 0;
-                    }
-                        ///После достижения тупика в этой ветке
-                    }
-                }
-            }
-            
-            //bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);//Ну типо перевернуть дерево))
-            return bmp;
-        }
-        /// <summary>
-        /// Метод прорисовки вершины графа
-        /// </summary>
-        /// <param name="g">Объект Graphics, в котором выполняется рисование</param>
-        /// <param name="pen">Перо, которым выполняется рисование</param>
-        /// <param name="text">Текст в вершине</param>
-        /// <param name="pos">Положение верхнего левого угла</param>
-        /// <param name="size">Размер</param>
-        void DrawVertex(Graphics g, Pen pen, string text, Point pos, Size size)
-        {
-            /// Прямоугольник
-            g.DrawRectangle(pen, new Rectangle(pos, size));
-            /// Текст, высотой в 3/5 высоты прямоугольника
-            g.DrawString(text, new Font(this.Font.Name, size.Height * 3f / 5f, this.Font.Style, GraphicsUnit.Pixel), Brushes.Black, pos);
-        }
-
-        /// <summary>
-        /// Метод прорисовки стрелок между вершинами графа
-        /// </summary>
-        /// <param name="g">Объект Graphics, в котором выполняется рисование</param>
-        /// <param name="pen">Перо, которым выполняется рисование</param>
-        /// <param name="pos1">Верхний левый угол вершины, из которой выходит стрелка</param>
-        /// <param name="pos2">Верхний левый угол вершины, в которую приходит стрелка</param>
-        /// <param name="size">Размер прямоугольника</param>
-        void DrawArrows(Graphics g, Pen pen, Point pos1, Point pos2, Size size)
-        {
-            Point pt1, pt2;
-            /// Положение точки на середине нижней стороны прямоугольника
-            pt1 = pos1; pt1.Offset(size.Width / 2, size.Height);
-            /// Положение точки на середине верхней стороны прямоугольника
-            pt2 = pos2; pt2.Offset(size.Width / 2, 0);
-            g.DrawLine(pen, pt1, pt2);
         }
     }
 }
